11c19cc5df3975662bbc9d10df2b9cb2d7f52310
runningpoem30 <aryaanandpathak30@gmail.com>
2025-08-12 01:12:14 +0530

    changes

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
-import { LuSun } from "react-icons/lu";
-import { LuMoon } from "react-icons/lu";
+// import { LuSun } from "react-icons/lu";
+// import { LuMoon } from "react-icons/lu";
+import { IoIosSunny } from "react-icons/io"
+import { IoMoon } from "react-icons/io5";
 
 
 // console.log(LuSun, LuMoon);
@@ -25,7 +27,7 @@ function Header() {
     <div>
         <div className="flex flex-col font-mono justify-start items-start px-4 sm:px-12 py-5 border-b border-b-2 border-gray-700 sm:flex-row gap-y-4 sm:gap-x-9 lg:gap-x-[1150px]">
   <h1 className="font-bold text-4xl sm:text-3xl"><Link to={'/home'}>Shortly</Link></h1>
-  <button onClick={() => setDarkMode(!darkMode)} className='lg:mt-3 lg:pb-2 lg:ml-[300px]'>{darkMode ? <LuSun size={26}/> : <LuMoon size={26}/>}</button>
+  <button onClick={() => setDarkMode(!darkMode)} className='lg:mt-3 lg:pb-2 lg:ml-[300px]'>{darkMode ? <IoIosSunny size={26}/> : <IoMoon size={26}/>}</button>
    </div>
     </div>
    
